3a02275b05b2ad4acae4bac309d1201e6e6c5df4
Kadek Arta <gsumiartawan@gmail.com>
2020-12-11 20:39:35 +0800

    Update Detail screen path

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,5 @@
 export { default as DenahScreen } from "./DenahScreen/DenahScreen";
-export { default as DetailScreen } from "./DetailScreen/DetailScreen";
+export { default as DetailScreen } from "./DenahScreen/DetailScreen";
 export { default as HomeScreen } from "./HomeScreen/HomeScreen";
 export { default as LaranganScreen } from "./LaranganScreen/LaranganScreen";
 export { default as ProfileScreen } from "./ProfileScreen/ProfileScreen";
