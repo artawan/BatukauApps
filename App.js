81c1942ca6f52bf42ccdac02ae1ab63fbf4a2f2d
Kadek Arta <gsumiartawan@gmail.com>
2021-01-12 22:46:57 +0800

    Move AppContainer function to App

--- App.js
+++ App.js
@@ -1,13 +1,69 @@
+import * as React from "react";
 import "react-native-gesture-handler";
-import React, { Component } from "react";
-import AppContainer from "./src/AppContainer";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { SplashScreen } from "expo";
+import * as Font from "expo-font";
+import Constants from "expo-constants";
+import useLinking from "./src/components/navigation/useLinking";
+import DrawerNavigation from "./src/components/DrawerNavigation/DrawerNavigation";
 
-class App extends Component {
-  render() {
-    return (
-        <AppContainer />
+const App = (props) =>{
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  const [initialNavigationState, setInitialNavigationState] = React.useState();
+  const containerRef = React.useRef();
+  const { getInitialState } = useLinking(containerRef);
+
+  // Load any resources or data that we need prior to rendering the app
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
+
+        // Load our initial navigation state
+        setInitialNavigationState(await getInitialState());
+
+        // Load fonts
+        await Font.loadAsync({
+          "balinese-family": require("./assets/font/BalineseFamilyNormal.ttf"),
+        });
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hide();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return null;
+  } else {
+    return(
+    <View style={styles.container}>
+        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+
+        <NavigationContainer
+          ref={containerRef}
+          initialState={initialNavigationState}
+        >
+          <DrawerNavigation />
+        </NavigationContainer>
+      </View>
     );
   }
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+  },
+});
+
 
 export default App;
